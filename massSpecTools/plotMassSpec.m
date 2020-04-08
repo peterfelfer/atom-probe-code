c5e272c93b946d8f8c6a08c755570fd753d42189
BenedictOtt <62551855+BenedictOtt@users.noreply.github.com>
2020-04-08 12:55:50 +0200

    plotMassSpec Update v2
    Updated combinations, deleted combination of rng and oldPlot, set default mode to 'count'.

--- massSpecTools/plotMassSpec.m
+++ massSpecTools/plotMassSpec.m
@@ -1,7 +1,8 @@
-function handle = plotMassSpec(mc, bin, mode, rng, oldPlot)
+function handle = plotMassSpec(mc, bin, mode)
 % plotMassSpec plots the data from pos to get a Massspectrum
 %
-% handle = plotMassSpec(mc, bin, mode, rng, oldPlot)
+% handle = plotMassSpec(mc, bin, mode)
+% handle = plotMassSpec(mc, bin)
 %
 % INPUTS
 % mc:       is the mass-to-charge(mc)-ratio [Da] of the events in the 
@@ -11,14 +12,16 @@ function handle = plotMassSpec(mc, bin, mode, rng, oldPlot)
 %           'count' records the number of counts
 %           'normalised' records the number of counts if the bin was one Da
 %           wide over the overall number of counts
-% rng:      loads existing ranges from rng if paresed before
-% oldPlot:  if old Plot is parsed, it will be deleted and replaced, zoom 
-%           limits will be kept
+%           default mode is 'count'
 % 
 % OUTPUTS
-%           figure that contains plot of counts or
+%           handle to the plot that contains counts or
 %           (counts/Dalton)/totalCounts over Dalton. Used in further
 %           analysis to find new ions
+
+if ~exist('mode','var')
+    mode = 'count';
+end
 
 if istable(mc)
     mc = mc.mc;
@@ -28,22 +31,7 @@ if length(mc(1,:)) > 1
     mc = mc(:,4);
 end
 
-%if old Plot is parsed, it will be deleted and replaced, zoom limits will
-%be kept
-if exist('oldPlot','var')
-    ax = get(oldPlot,'Parent');
-    XLim = ax.XLim;
-    plots = get(get(oldPlot,'Parent'),'Children');
-    x = oldPlot.XData;
-    y = oldPlot.YData;
-    delete(plots);
-    handle = area(ax,x,y,'FaceColor',[.9 .9 .9]);
-    handle.UserData.plotType = "massSpectrum";
-    hold on;
-    
-    f = get(ax,'Parent');
-    
-else
+
     f = figure('Name','mass spectrum');
     ax = axes(f);
     
@@ -51,13 +39,14 @@ else
     
     x = linspace(0,mcmax,round(mcmax/bin));
     
-    if strcmp(mode,'count')
+    if  strcmp(mode,'count')
         y = hist(mc,x);
     elseif strcmp(mode,'normalised')
         % calculate as counts/(Da * totalCts) so that mass spectra with different
         % count numbers are comparable
         y = hist(mc,x) / bin / length(mc);
         %med = median(y);
+    else y = hist(mc,x);
     end
     
     % plot all mass spectrum
@@ -91,34 +80,6 @@ else
     t.Position = [.15 .8 .27 .1];
     pan xon
     zoom xon
-end
-
-
-
-
-
-if exist('rng','var')
-    %populate with plots of ranges
-    for r=1:length(rng)
-        inRng = (x>=rng(r).mcbegin) & (x<rng(r).mcend);
-        rngPlotHandles(r) = area(x(inRng),y(inRng),'Parent',ax);
-        rngPlotHandles(r).FaceColor = rng(r).color;
-        rngPlotHandles(r).DisplayName = rng(r).rangeName;
-        rngPlotHandles(r).UserData.plotType = "range";
-        
-        %find peak and add label
-        peakloc = find(y(inRng) == max(y(inRng)));
-        peakloc = peakloc(1);
-        locs = y(inRng);
-        locs = locs(peakloc);
-        
-        th(r) = text(rng(r).mcbegin,max(y(inRng)) * 1.25,rng(r).rangeName,'clipping','on');
-        %th.Color = [.5 .5 .5];
-        th(r).UserData.plotType = "rangeName";
-        
-        
-    end
-end
 
 handle.DisplayName = 'mass spectrum';
 
