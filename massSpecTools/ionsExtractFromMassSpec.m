454d890e2d5b34545e85f0f521d4fba8d0f9107d
ValentinAPT <62693841+ValentinAPT@users.noreply.github.com>
2020-04-21 09:03:07 +0200

    final changes

--- massSpecTools/ionsExtractFromMassSpec.m
+++ massSpecTools/ionsExtractFromMassSpec.m
@@ -1,13 +1,16 @@
+% function worked properly, however after changing the function name
+% of "convertIonName" to the correct name "ionConvertNameTable" (line 33),
+% function does not work as long as ionConvertNameTable is not updated
+
 function ionTable = ionsExtractFromMassSpec(spec)
 % pulls all ions and corresponding information from a mass spectrum plot
-
-% get all plots connected to the mass spectrum
+% gets all plots connected to the mass spectrum
 %
 % INPUT: spec, area plot that displays the mass spectrum (histogram of m/c frequencies) 
 %        either in raw counts or normalised to bin width and total ion count
 %
 % OUTPUT: ionTable, table with allocated ions and additional information
-%         (charge state,
+%         (charge state, corresponding color code)
 %
 
 plots = spec.Parent.Children;
@@ -27,7 +30,7 @@ for pl = 1:length(plots)
     if type == "ion"
         ion{idx,:} = plots(pl).UserData.ion{1}.element; % do not extract isotope information
         chargeState(idx,:) = plots(pl).UserData.chargeState(1);
-        ionName{idx,:} = convertIonName(plots(pl).UserData.ion{1}.element);
+        ionName{idx,:} = ionConvertNameTable(plots(pl).UserData.ion{1}.element);
         color(idx,:) = plots(pl).Color;
         
         idx = idx + 1;
