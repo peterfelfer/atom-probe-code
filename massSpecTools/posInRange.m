a7ef0d144b74c5551bde1e5060cd2535ae1f4d42
ValentinAPT <62693841+ValentinAPT@users.noreply.github.com>
2020-05-07 13:02:21 +0200

    finished code and documentation
    changes were necessary due to the change to the table-based pos file
    (length --> height & dot indexing)

--- massSpecTools/posInRange.m
+++ massSpecTools/posInRange.m
@@ -1,10 +1,24 @@
-function pos = posInRange(pos,rng)
-%gives all ions within a given range [mcbeg1, mcend1; mcbeg2, mcend2 ...
+@Martina and/or Benedict: Please delete the following lines, they are
+just a hint for you.
+Be aware, that the input variable "rng" has nothing to do with the
+output variable of the function readrrng (often used like rng = readrrng)!
 
-isIn = false(length(pos(:,1)),1);
+function pos = posInRange(pos,rng)
+% gives all ions within a given range [mcbeg1, mcend1; mcbeg2, mcend2 ...
+%
+% INPUT:
+%   pos: table, pos file with .mc field
+%   rng: array, pairs of begin and end values of mc [mcbeg1, mcend1;
+%        mcbge2, mcend2; ...]
+%
+% OUTPUS:
+%   pos: table, pos file with only the ions within the ranges of rng
+%
+
+isIn = false(height(pos(:,1)),1);
 
 for r = 1:length(rng(:,1))
-    in = pos(:,4)>=rng(r,1) & pos(:,4)<=rng(r,2);
+    in = pos.mc>=rng(r,1) & pos.mc<=rng(r,2);
     isIn = isIn | in;
 end
 
