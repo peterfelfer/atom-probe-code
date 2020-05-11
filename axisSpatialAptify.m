cf5fadfd458845af2e4b7abc734ddcecc1bf3937
BenedictOtt <62551855+BenedictOtt@users.noreply.github.com>
2020-05-11 12:35:58 +0200

    tested, color reset on white, view reset on 3D upper front corner

--- axisSpatialAptify.m
+++ axisSpatialAptify.m
@@ -5,12 +5,14 @@ function axisSpatialAptify
 
 axis equal;
 rotate3d on;
+view(3);
 
 set(gca,'Box','On');
 set(gca,'BoxStyle','full');
 set(gca,'XColor',[1 0 0]);
 set(gca,'YColor',[0 1 0]);
 set(gca,'ZColor',[0 0 1]);
+set(gca,'Color',[1 1 1]);
 
 set(gca,'ZDir','reverse');
 
