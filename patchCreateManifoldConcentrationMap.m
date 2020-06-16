6a06818e34f99734a66ac80eae93ff4ab918e0b7
ValentinAPT <62693841+ValentinAPT@users.noreply.github.com>
2020-06-16 15:59:37 +0200

    Benedict was right, no dot notation in code since pos was converted into double array

--- patchCreateManifoldConcentrationMap.m
+++ patchCreateManifoldConcentrationMap.m
@@ -24,11 +24,11 @@ normals = patchnormals(fv);
 %% calculate per vertex count atoms
 % calculate which vertex is closest to an atom
 closest = dsearchn(fv.vertices,pos);
-distVec = [pos.x, pos.y, pos.z] - fv.vertices(closest,:);
+distVec = pos(:,1:3) - fv.vertices(closest,:);
 dist = sum(normals(closest,:) .* distVec,2);
 
 closestP = dsearchn(fv.vertices,parentPos);
-distVecP = [parentPos.x, parentPos.y, parentPos.z] - fv.vertices(closestP,:);
+distVecP = parentPos(:,1:3) - fv.vertices(closestP,:);
 distP = sum(normals(closestP,:) .* distVecP,2);
 
 % clip by distance
